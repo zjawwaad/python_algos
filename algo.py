b8b18fc2befd9eafff6c99f275c95b057cfb1153
zjawwaad <zjawwaad@yahoo.com>
2023-05-24 11:28:35 -0400

    strings w loops

--- algo.py
+++ algo.py
@@ -64,3 +64,49 @@ def not_string(s):
 def not_string(s):
     return s if s.startswith("not") else "not " + s
 
+
+def missing_char(str, n):
+    front = str[:n]
+    back = str[n+1:]
+    return front + back
+
+
+def front_back(s):
+    if len(s) >= 2:
+        return s[-1] + s[1:-1] + s[0]
+    else:
+        return s
+
+s = "hello"
+# The last character: "o"
+last_char = s[-1]
+
+# The slice of characters from index 1 to -1 (excluding the first and last): "ell"
+middle_chars = s[1:-1]
+
+# The first character: "h"
+first_char = s[0]
+
+# Concatenating the parts together: "oellh"
+result = last_char + middle_chars + first_char
+
+
+def front_back(str):
+    if len(str) >= 2:
+        return str[-1] + str[1:-1] + str[0]
+    else:
+        return str
+
+
+
+# Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+
+def front3(str):
+    if str >= 3:
+        return str[0:3]*3
+    else:
+        return str*3
+
+def string_times(str, n):
+    return str*n
